b621fec376dd3b8b686d868d5241a338bb85e59b
Alejandro González Hevia <alejandrgh11@gmail.com>
2020-06-22 18:05:27 +0200

    Load of dataset 2 from API and initial XML parsing

---
+++ src/data_reader.py
@@ -0,0 +1,22 @@
+""" Functions used to fetch datasets related to the publications track
+"""
+
+
+def fetch_covid_dataset():
+    pass
+
+
+def fetch_pmc_dataset():
+    pass
+
+
+def fetch_pmc_article(pmc_id):
+    """ Fetchs an article from Europe PMC with the given id.
+
+    Parameter
+    ---------
+    pcm_id : str
+        String with the id of the article to be fetched. It must be of the
+        form 'PCXXXXXXX'.
+    """
+    pass
