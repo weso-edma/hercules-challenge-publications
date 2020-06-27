f6127256399a3c7c24d3f16d3ae7398f5d4c5682
alejgh <alejandrgh11@gmail.com>
2020-06-27 08:26:57 -0400

    Reuse common library and separate data fetching and exploration

---
+++ notebooks/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import os
+import sys
+
+# set up module paths for imports
+module_path = os.path.abspath(os.path.join('..'))
+sys.path.append(module_path)
+src_path = os.path.abspath(os.path.join('..', 'src'))
+sys.path.append(src_path)
+
+# start logging system and set logging level
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logging.info("Starting logger")
+
+DATA_DIR = os.path.join(module_path, 'data')
+RESULTS_DIR = os.path.join(module_path, 'results')
